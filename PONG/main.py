33891f1d3a01cd239d5e0d4e6b646b45187fc135
Lucas <101103572+meislucas@users.noreply.github.com>
2022-06-16 17:18:10 +0200

    Added files

---
+++ PONG/main.py
@@ -0,0 +1,40 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.title("PONG")
+screen.setup(width=800, height=600)
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.move_up, "Up")
+screen.onkey(r_paddle.move_down, "Down")
+screen.onkey(l_paddle.move_up, "w")
+screen.onkey(l_paddle.move_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(ball.move_speed)
+    ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 330 or ball.distance(l_paddle) < 50 and ball.xcor() < -330:
+        ball.bounce_x()
+    if ball.xcor() > 380:
+        scoreboard.l_point()
+        ball.reset_score()
+    if ball.xcor() < -380:
+        scoreboard.r_point()
+        ball.reset_score()
+
+screen.exitonclick()
